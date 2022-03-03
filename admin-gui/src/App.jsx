57999016fa90e490f9ef965eb996aa1d4618b7b9
Filip <filip.ahlander@gmail.com>
2022-03-03 21:16:44 +0100

    add react with tailwind

---
+++ admin-gui/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-3xl font-bold underline">No Craco</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ admin-gui/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <>
+      <h1>Spaper</h1>
+      <nav className="bg-gray-700 h-10">
+        <ul>
+          <li className="text-xl">Link1</li>
+          <li>Link2</li>
+        </ul>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
